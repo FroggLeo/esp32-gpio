538f5de22ecfdf1869a187284c13205bf4c96827
Leo <88123479+FroggLeo@users.noreply.github.com>
2026-09-13 02:16:33 -0700

    add buzzer functionality

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,9 @@
 #include "esp_log.h"
 #include "hal/adc_types.h"
 #include "hal/gpio_types.h"
+#include "hal/ledc_types.h"
 #include "pins.hpp"
+#include "soc/clk_tree_defs.h"
 #include "soc/gpio_num.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -11,15 +13,19 @@
 #include <cstdint>
 
 static const char* TAG = "main";
+static constexpr bool debug = false;
 
 gpio_num_t buzzer_pin = Board::D7;
-gpio_num_t button_big_pin = Board::D8;
-gpio_num_t button_stack_pin = Board::D9;
-adc_channel_t button_stack_adc_pin = ADC_CHANNEL_7;
-
 gpio_config_t buzzer = {};
+ledc_timer_config_t buzzer_timer = {};
+ledc_channel_config_t buzzer_channel = {};
+
+gpio_num_t button_big_pin = Board::D8;
 gpio_config_t button_big = {};
+
+gpio_num_t button_stack_pin = Board::D9;
 gpio_config_t button_stack = {};
+adc_channel_t button_stack_adc_pin = ADC_CHANNEL_7;
 
 void setup_gpio() {
     buzzer.pin_bit_mask = 1ULL << buzzer_pin;
@@ -57,6 +63,34 @@ void setup_adc() {
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle,button_stack_adc_pin,&channel_config));
 }
 
+void setup_buzzer() {
+    buzzer_timer.speed_mode = LEDC_LOW_SPEED_MODE;
+    buzzer_timer.timer_num = LEDC_TIMER_0;
+    buzzer_timer.duty_resolution = LEDC_TIMER_10_BIT;
+    buzzer_timer.freq_hz = 1000;
+    buzzer_timer.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&buzzer_timer);
+    buzzer_channel.gpio_num = buzzer_pin;
+    buzzer_channel.speed_mode = LEDC_LOW_SPEED_MODE;
+    buzzer_channel.channel = LEDC_CHANNEL_0;
+    buzzer_channel.intr_type = LEDC_INTR_DISABLE;
+    buzzer_channel.timer_sel = LEDC_TIMER_0;
+    buzzer_channel.duty = 0;
+    buzzer_channel.hpoint = 0;
+    ledc_channel_config(&buzzer_channel);
+}
+
+void buzzer_tone(uint32_t f) {
+    ledc_set_freq(LEDC_LOW_SPEED_MODE, LEDC_TIMER_0, f);
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 512);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+}
+
+void buzzer_stop() {
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+}
+
 void delay(int ms) {
     vTaskDelay(pdMS_TO_TICKS(ms));
 }
@@ -64,14 +98,14 @@ void delay(int ms) {
 int8_t button_stack_read() {
     int v = 4000;
     adc_oneshot_read(adc_handle, button_stack_adc_pin, &v);
-    ESP_LOGI(TAG, "Button stack value: %d", v);
-    if (v < 500) {
+    if (debug) {ESP_LOGI(TAG, "Button stack value: %d", v);}
+    if (v < 600) {
         return 0;
-    } else if (v < 1500) {
+    } else if (v < 1600) {
         return 1;
-    } else if (v < 2100) {
+    } else if (v < 2150) {
         return 2;
-    } else if (v < 2500) {
+    } else if (v < 3000) {
         return 3;
     }
     return -1;
@@ -80,13 +114,25 @@ int8_t button_stack_read() {
 extern "C" void app_main() {
     setup_gpio();
     setup_adc();
+    setup_buzzer();
     while (true) {
         ESP_LOGI(TAG, "Button pressed: %d", !gpio_get_level(button_big_pin));
         int8_t pressed_button = button_stack_read();
         if (pressed_button != -1) {
             ESP_LOGI(TAG, "Button stack pressed: button %d", pressed_button);
         }
-        delay(500);
+        if (pressed_button == 0) {
+            buzzer_tone(500);
+        } else if (pressed_button == 1) {
+            buzzer_tone(750);
+        } else if (pressed_button == 2) {
+            buzzer_tone(1000);
+        } else if (pressed_button == 3) {
+            buzzer_tone(1500);
+        } else {
+            buzzer_stop();
+        }
+        delay(50);
     }
 
 

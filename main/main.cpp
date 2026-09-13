68accb5ab8ee0b78a5a3392a2bc8e44587c484f6
Leo <88123479+FroggLeo@users.noreply.github.com>
2026-09-12 23:40:33 -0700

    initialize project and add .gitignore

---
+++ main/main.cpp
@@ -0,0 +1,6 @@
+#include "esp_log.h"
+#include "pins.hpp"
+
+extern "C" void app_main() {
+    
+}
---
+++ main/pins.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "soc/gpio_num.h"
+
+namespace Board {
+    inline constexpr gpio_num_t D0 = GPIO_NUM_1;
+    inline constexpr gpio_num_t D1 = GPIO_NUM_2;
+    inline constexpr gpio_num_t D2 = GPIO_NUM_3;
+    inline constexpr gpio_num_t D3 = GPIO_NUM_4;
+    inline constexpr gpio_num_t D4 = GPIO_NUM_5;
+    inline constexpr gpio_num_t D5 = GPIO_NUM_6;
+    inline constexpr gpio_num_t D6 = GPIO_NUM_43;
+
+    inline constexpr gpio_num_t D7 = GPIO_NUM_44;
+    inline constexpr gpio_num_t D8 = GPIO_NUM_7;
+    inline constexpr gpio_num_t D9 = GPIO_NUM_8;
+    inline constexpr gpio_num_t D10 = GPIO_NUM_9;
+}

3232ea55b44e84a97891517093e18cc2649ad1b9
Leo <88123479+FroggLeo@users.noreply.github.com>
2026-09-21 01:47:27 -0700

    add generic max98357a code and add ec11 implementation

--- main/main.cpp
+++ main/main.cpp
@@ -23,9 +23,10 @@ ledc_channel_config_t buzzer_channel = {};
 gpio_num_t button_big_pin = Board::D8;
 gpio_config_t button_big = {};
 
-gpio_num_t button_stack_pin = Board::D9;
-gpio_config_t button_stack = {};
-adc_channel_t button_stack_adc_pin = ADC_CHANNEL_7;
+gpio_num_t ec11_pin1 = Board::D10;
+gpio_num_t ec11_pin2 = Board::D9;
+gpio_num_t ec11_pin3 = Board::D2;
+gpio_config_t ec11 = {};
 
 void setup_gpio() {
     buzzer.pin_bit_mask = 1ULL << buzzer_pin;
@@ -40,16 +41,36 @@ void setup_gpio() {
     button_big.pull_down_en = GPIO_PULLDOWN_DISABLE;
     button_big.intr_type = GPIO_INTR_DISABLE;
     gpio_config(&button_big);
-    /*button_stack.pin_bit_mask = 1ULL << button_stack_pin;
-    button_stack.mode = GPIO_MODE_INPUT;
-    button_stack.pull_up_en = GPIO_PULLUP_DISABLE;
-    button_stack.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    button_stack.intr_type = GPIO_INTR_DISABLE;
-    gpio_config(&button_stack);*/
+    ec11.pin_bit_mask = 1ULL << ec11_pin1;
+    ec11.mode = GPIO_MODE_INPUT;
+    ec11.pull_up_en = GPIO_PULLUP_ENABLE;
+    ec11.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    ec11.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&ec11);
+    ec11.pin_bit_mask = 1ULL << ec11_pin2;
+    gpio_config(&ec11);
+    ec11.pin_bit_mask = 1ULL << ec11_pin3;
+    gpio_config(&ec11);
 }
 
-adc_oneshot_unit_handle_t adc_handle;
+int8_t check_ec11(gpio_num_t pin1, gpio_num_t pin2) {
+    bool curr_pin1 = gpio_get_level(pin1);
+    bool curr_pin2 = gpio_get_level(pin2);
+    static bool prev_pin1 = curr_pin1; // set current read only on first run
+    int8_t direction = 0;
+    if (!curr_pin1 && prev_pin1) { // true when current read is LOW and previous read is HIGH
+        if (curr_pin2) { // if pin2 is HIGH
+            direction = 1;
+        } else {
+            direction = -1;
+        }
+    }
+    prev_pin1 = curr_pin1;
+    return direction;
+}
 
+/* unused
+adc_oneshot_unit_handle_t adc_handle;
 void setup_adc() {
     adc_oneshot_unit_init_cfg_t init_config = {};
     init_config.unit_id = ADC_UNIT_1;
@@ -63,6 +84,22 @@ void setup_adc() {
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle,button_stack_adc_pin,&channel_config));
 }
 
+int8_t button_stack_read() {
+    int v = 4000;
+    adc_oneshot_read(adc_handle, button_stack_adc_pin, &v);
+    if (debug) {ESP_LOGI(TAG, "Button stack value: %d", v);}
+    if (v < 600) {
+        return 0;
+    } else if (v < 1600) {
+        return 1;
+    } else if (v < 2150) {
+        return 2;
+    } else if (v < 3000) {
+        return 3;
+    }
+    return -1;
+}
+*/
 void setup_buzzer() {
     buzzer_timer.speed_mode = LEDC_LOW_SPEED_MODE;
     buzzer_timer.timer_num = LEDC_TIMER_0;
@@ -95,27 +132,11 @@ void delay(int ms) {
     vTaskDelay(pdMS_TO_TICKS(ms));
 }
 
-int8_t button_stack_read() {
-    int v = 4000;
-    adc_oneshot_read(adc_handle, button_stack_adc_pin, &v);
-    if (debug) {ESP_LOGI(TAG, "Button stack value: %d", v);}
-    if (v < 600) {
-        return 0;
-    } else if (v < 1600) {
-        return 1;
-    } else if (v < 2150) {
-        return 2;
-    } else if (v < 3000) {
-        return 3;
-    }
-    return -1;
-}
-
 extern "C" void app_main() {
     setup_gpio();
-    setup_adc();
     setup_buzzer();
-    while (true) {
+
+    /*while (true) {
         ESP_LOGI(TAG, "Button pressed: %d", !gpio_get_level(button_big_pin));
         int8_t pressed_button = button_stack_read();
         if (pressed_button != -1) {
@@ -133,7 +154,7 @@ extern "C" void app_main() {
             buzzer_stop();
         }
         delay(50);
-    }
+    }*/
 
 
 }
